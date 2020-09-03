3e8acf2a58e981251711a7aa86f99c4ab4be73aa
zhoux <zhou7469@gmail.com>
2020-09-03 21:24:18 +0800

    no message

---
+++ java/src/main/java/com/smile/App.java
@@ -0,0 +1,13 @@
+package com.smile;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
