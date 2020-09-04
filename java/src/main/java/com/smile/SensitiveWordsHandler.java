32c475aca3938e0728ba8762e8d5b4e050fd5b9c
zhoux <zhou7469@gmail.com>
2020-09-04 10:16:31 +0800

    no message

---
+++ java/src/main/java/com/smile/SensitiveWordsHandler.java
@@ -0,0 +1,31 @@
+package com.smile;
+
+/**
+ * 敏感词管理
+ */
+public class SensitiveWordsHandler
+{
+    /*
+    加载配置
+     */
+    public static void init(String cfgPath){
+
+    }
+
+    /*
+    依据配置需要替换字符串，替换敏感词
+     */
+    public static String filter(String content){
+
+        return content;
+    }
+
+    /*
+    判断该内容是否包含敏感词
+     */
+    public static boolean hadSensitiveWords(String content){
+
+        return false;
+    }
+
+}
